eb9f4ce864d63415f37544fc30b21fc404750d19
cyning <449598613@qq.com>
2020-06-03 23:48:59 +0800

    01_小程序初体验

---
+++ pages/teachers/teachers.js
@@ -0,0 +1,73 @@
+// pages/teachers/teachers.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        teacherObj: {
+            t1: ["http://img0.imgtn.bdimg.com/it/u=2331078166,2619950172&fm=26&gp=0.jpg", "哈课-张三"],
+            t2: ["http://img3.imgtn.bdimg.com/it/u=2677697676,1575147477&fm=26&gp=0.jpg", "哈课-李四"],
+            t3: ["http://img3.imgtn.bdimg.com/it/u=2089311874,1939731305&fm=26&gp=0.jpg", "哈课-小不点"],
+            t4: ["http://img0.imgtn.bdimg.com/it/u=111421943,1694088817&fm=26&gp=0.jpg", "哈课-牛牛牛"],
+            t5: ["http://img3.imgtn.bdimg.com/it/u=3906341877,414074668&fm=26&gp=0.jpg", "哈课-傻瓜"],
+            t6: ["http://img0.imgtn.bdimg.com/it/u=3151693060,1796009580&fm=26&gp=0.jpg", "哈课-Excel"]
+        } 
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
